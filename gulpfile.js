69249ac0ab556c50a6dbfb0ebf4888d48577e397
Vagelis <drz4007@gmail.com>
2016-06-19 12:21:15 +0300

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./www"
+        }
+    });
+    gulp.watch('./www/sass/**/*.scss', ['sass']);
+    gulp.watch('./www/css/**/*.css').on('change', browserSync.reload);
+    gulp.watch('./www/controllers/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./www/index.html').on('change', browserSync.reload);
+});
 
 gulp.task('sass', function () {
     return gulp.src('./www/sass/**/*.scss')
@@ -13,6 +27,4 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./www/css'));
 });
 
-gulp.task('default', function () {
-    gulp.watch('./www/sass/**/*.scss', ['sass']);
-});
+gulp.task('default', ['browser-sync']);
--- www/controllers/mainCtrl.js
+++ www/controllers/mainCtrl.js
@@ -1,20 +1,31 @@
-angular.module('MyApp',[])
-    .controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
-        $scope.name = 'Me!';
-        $scope.request = 'null';
+angular.module('MyApp', [])
+    .controller('mainCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
 
-        // Simple GET request example:
         $http({
             method: 'GET',
-            url: 'http://stats.nba.com/leaders/#!/?StatCategory=PTS'
+            url: 'https://public-api.wordpress.com/rest/v1.1/sites/peribiblicum.wordpress.com/posts/?number=100&fields=title,content'
         }).then(function successCallback(response) {
-            // $scope.request = 'The request to NBA stats was succesfull.';
-            $scope.request = response;
-        }, function errorCallback(response) {
-            $scope.request = 'The request to NBA stats failed.';
+            $scope.posts = response.data.posts;
+
+        }, function errorCallback() {
+            $scope.error = 'An error occurred with the http request.';
         });
 
+        // setTimeout(function () {
+        //     for (var i = 0; i < $scope.posts.length; i++) {
+        //         $log.info($scope.posts[i].title);
+        //     }
+        // }, 3000);
 
+
+        $scope.toggleCustom = function (title) {
+            for (var i = 0; i < $scope.posts.length; i++) {
+                // lconsole.log($scope.posts[i].title);
+                if ($scope.posts[i].title === title) {
+                    $scope.posts[i].show = $scope.posts[i].show != true;
+                }
+            }
+        };
 
 
     }]);
