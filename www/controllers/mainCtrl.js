6317a60a5801bbb6db1c74375a952415a69bf14d
Vagelis <drz4007@gmail.com>
2016-06-23 12:51:50 +0300

    Ready

--- www/controllers/mainCtrl.js
+++ www/controllers/mainCtrl.js
@@ -1,26 +1,31 @@
 angular.module('MyApp', [])
-    .controller('mainCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
+    .controller('mainCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+
+        // Use the following to show and hide the black overlay
+        // while fetching data.
+        $scope.loading = true;
+
+        var date = new Date().getFullYear();
+        $scope.date = date;
 
         $http({
             method: 'GET',
-            url: 'https://public-api.wordpress.com/rest/v1.1/sites/peribiblicum.wordpress.com/posts/?number=100&fields=title,content'
+            url: 'https://public-api.wordpress.com/rest/v1.1/sites/peribiblicum.wordpress.com/posts/?number=10&fields=title,content'
         }).then(function successCallback(response) {
             $scope.posts = response.data.posts;
+            $scope.loading = false;
 
         }, function errorCallback() {
             $scope.error = 'An error occurred with the http request.';
+            $scope.loading = false;
         });
 
-        // setTimeout(function () {
-        //     for (var i = 0; i < $scope.posts.length; i++) {
-        //         $log.info($scope.posts[i].title);
-        //     }
-        // }, 3000);
-
+        $scope.toTrustedHTML = function (html) {
+            return $sce.trustAsHtml(html);
+        };
 
         $scope.toggleCustom = function (title) {
             for (var i = 0; i < $scope.posts.length; i++) {
-                // lconsole.log($scope.posts[i].title);
                 if ($scope.posts[i].title === title) {
                     $scope.posts[i].show = $scope.posts[i].show != true;
                 }
